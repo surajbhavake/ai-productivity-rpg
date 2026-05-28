b91f3bfff4a7f7a6a94d5cc5e92e512c81b93368
surajbhavake <surajbhavake2@gmail.com>
2026-05-29 01:58:36 +0530

    chore : updated the authentication model

--- backend/apps/authentication/models.py
+++ backend/apps/authentication/models.py
@@ -63,3 +63,96 @@ class User(AbstractUser):
 
     updated_at =  models.DateTimeField(auto_now=True)
 
+
+
+    class Meta : 
+        db_table = 'user'
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+        
+    def __str__(self):
+        return f"{self.username}(Level {self.level})"
+    
+
+    #--------PROPERTIES(COMPUTABLE FIELDS)---------
+
+    @property  #using function as a variable
+    def xp_to_next_level(self):
+        return self.level * 100
+    
+    @property
+    def xp_progress_percentage(self):
+        if self.xp_to_next_level == 0:
+            return 100
+        return int((self.xp/self.xp_to_next_level)*100)
+    @property
+    def rank_title(self):
+        ranks = {
+            range(1,5):"Apprentice",
+            range(5,10):"Journeyman",
+            range(10,20):"Expert",
+            range(20,30):"Master",
+            range(30,50):"Grandmaster",
+            range(50,101):"Legend",
+        
+        }
+        for  level_range,title in ranks.items():
+            if self.level in level_range:
+                return title
+            
+        return "Transendent"
+    
+    #------METHODS(BUSINESS LOGIC)-------
+
+    def add_xp(self,amount,source="task"):
+        self.xp += amount
+        self.total_xp_earned += amount
+
+        leveled_up = False
+        new_level = self.level
+
+        while self.xp >= self.xp_to_next_level:
+            self.xp -= self.xp_to_next_level
+            self.level += 1
+            leveled_up = True
+            new_level = self.level
+
+        self.save()
+
+        return{
+            'leveled_up':leveled_up,
+            'new_level':new_level,
+            'current_xp':self.xp,
+            'xp_added':amount
+        }
+    
+    def update_streak(self):
+
+        today = timezone.now().date()
+
+        if self.last_activity_date is None:
+            self.current_streak = 1
+
+        elif self.last_activity_date == today :
+            pass
+
+        elif (today - self.last_activity_date).days == 1:
+            self.current_streak +=1
+        else :
+            self.current_streak = 1
+
+        #longest streak
+
+        if self.current_streak > self.longest_streak:
+            self.longest_streak = self.current_streak
+
+        self.last_activity_date = today
+        self.save()
+
+    
+
+
+
+
+
+
